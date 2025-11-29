7f99d43d72e7ccb59e22784fcdf0ea88c4ad4fff
Paul Otieno <vitalispaul48@live.com>
2025-11-30 02:00:23 +0300

    Add logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <limits>
+
+// Ask the user for a year and determine if it’s a leap year.
+
+/*
+ * 1. Create isLeapYear function. Takes a year as the parameter
+ * 2. Ask user for input and validate that input is a number.
+ * 2. Check if year is divisible by 4
+ * 3. Return result
+ *
+ */
+
+bool isLeapYear(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int inputYear = 0;
+
+    while (true) {
+        std::cout << "Enter a year: ";
+
+        if (!(std::cin >> inputYear)) {
+            std::cout << "Kindly enter a valid year" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        break;
+    }
+
+    std::cout << std::boolalpha << isLeapYear(inputYear);
     return 0;
 }
